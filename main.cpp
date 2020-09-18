0277d8e2cd4270b654f04d3247eb9a7add217a30
JonathanZhao3033 <303324025@qq.com>
2020-09-18 11:50:21 +0800

    20200918

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Sales_item.h"
+#include <windows.h> 
+int main()
+{
+Sales_item book;
+std::cout << "enter bookname number and price" << std::endl;
+std::cin >> book;
+std::cout << book << std::endl;
+std::cout << book.isbn() << std::endl;
+system("pause");
+    return 0;
+//jfapfieawf
+}
